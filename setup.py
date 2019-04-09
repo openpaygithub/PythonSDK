c1caffc2bfdadfaebaee75cd47a92c10c6fba209
Samiul Sk <>
2019-04-09 13:51:26 +0530

    version changed for pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-openpay",
-    version="0.6",
+    version="0.6.1",
     author="openpay",
     install_requires=[
         'lxml',
